4be2ed087258ac6af84ae01832b907ae9f1cb96b
Adil Munawar <adilfromavengers@gmail.com>
2025-10-26 11:03:56 +0000

    instead of this background you have to go with old spline background and

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Spline from '@splinetool/react-spline/next';
 import Navigation from '@/components/Navigation';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
@@ -5,12 +6,24 @@ import SkillsSection from '@/components/SkillsSection';
 import ServicesSection from '@/components/ServicesSection';
 import ProjectsSection from '@/components/ProjectsSection';
 import ContactSection from '@/components/ContactSection';
-import ReactiveTileBackground from '@/components/ReactiveTileBackground';
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-cyber-dark">
-      <ReactiveTileBackground />
+      <div style={{
+        width: '105vw',
+        height: '105vh',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%) scale(1.1)',
+        zIndex: 0,
+        pointerEvents: 'none'
+      }}>
+        <Spline
+          scene="https://prod.spline.design/AvF-dvRCaMAo12qX/scene.splinecode"
+        />
+      </div>
       <Navigation />
       
       <main className="relative z-10">

e4159f105da622902ed74d32401a8044b27a8e2b
Adil Munawar <adilfromavengers@gmail.com>
2025-10-26 14:17:14 +0000

    Delete the unused component files:
    Bash
    
    rm src/components/LiquidEther.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import Spline from '@splinetool/react-spline/next';
 import Navigation from '@/components/Navigation';
-import LiquidEther from '@/components/LiquidEther';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
 import SkillsSection from '@/components/SkillsSection';
@@ -10,30 +10,18 @@ import ContactSection from '@/components/ContactSection';
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-cyber-dark">
-      <div style={{ 
-        width: '100vw', 
-        height: '100vh', 
-        position: 'fixed', 
-        top: 0, 
-        left: 0, 
-        zIndex: -1 
+      <div style={{
+        width: '105vw',
+        height: '105vh',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%) scale(1.2)',
+        zIndex: 0,
+        pointerEvents: 'none'
       }}>
-        <LiquidEther
-          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={false}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.5}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.5}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={3000}
-          autoRampDuration={0.6}
+        <Spline
+          scene="https://prod.spline.design/AvF-dvRCaMAo12qX/scene.splinecode"
         />
       </div>
       <Navigation />

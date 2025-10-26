0fb24338d9e4b239e6de02fc2855091eed3bdcb6
Adil Munawar <adilfromavengers@gmail.com>
2025-10-26 08:57:30 +0000

    you have to change the background with such realistic effect I will prov

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14466cb17420f0e5"; }
+/******/ 		__webpack_require__.h = function() { return "8573afc3a8aa63c4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import Navigation from '@/components/Navigation';
+import LiquidEther from '@/components/LiquidEther';
+import HeroSection from '@/components/HeroSection';
+import AboutSection from '@/components/AboutSection';
+import SkillsSection from '@/components/SkillsSection';
+import ServicesSection from '@/components/ServicesSection';
+import ProjectsSection from '@/components/ProjectsSection';
+import ContactSection from '@/components/ContactSection';
+
+export default function Home() {
+  return (
+    <div className="relative min-h-screen bg-cyber-dark">
+      <div className="absolute top-0 left-0 w-full h-full z-0">
+        <LiquidEther
+          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
+      <Navigation />
+      
+      <main className="relative z-10">
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ServicesSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+    </div>
+  );
+}
